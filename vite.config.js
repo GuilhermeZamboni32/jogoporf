bd1af13fae771e83c0fbe50733b61e3420770a89
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2025-02-18 16:46:39 -0300

    teste3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Jogoprof',
+        short_name: 'JP',
+        description: 'Um jogo de emojis',
+        theme_color: 'image/png',
+        icons: [
+          {
+            src: 'axiloti192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'axiloti512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
