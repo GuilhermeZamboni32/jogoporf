86a09f334f173750b23424c6989d71bb19405c41
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2025-02-18 15:23:06 -0300

    teste1

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [emojis, setEmojis] = useState([
+    "😀", "😁", "😂", "🤣", "😃", "😄", "😅", "😆", "😉", "😊", "😋", "😎", "😍", "😘", "🥰", "😗", "😙", "🥲", "😚", "☺️", 
+    "🙂", "🤗", "🤩", "🤔", "🫡", "🤨", "😐", "😑", "😶", "🫥", "😶‍🌫️", "🙄", "😏", "😣", "😥", "😮", "🤐", "😯", "😪", "😫", 
+    "🥱", "😴", "😌", "😛", "😜", "😝", "🤤", "😒", "🥳", "🤠", "🤑", "🤓", "🤯", "🥺", "🫣", "🫠", "😵", "😇"
+  ]);
+  const [emoji, setEmoji] = useState('❤️')
+  
+  function sortearEmoji(){
+    if(emojis.length > 0){
+      let i = Math.floor(Math.random() * emojis.length)
+      setEmoji(emojis[i])
+      setEmojis([...emojis.filter((e) => e != emoji[i])])
+      console.log(emojis.length)
+
+    }else{
+      alert(acabo)
+
+    }
+
+  }
+
+  return (
+    <div className='container-app'>
+      <p className='emoji' onClick={sortearEmoji}>{emoji}</p>
+      {/*emojis*/}
+
+    </div>
+  )
+}
+
+export default App
